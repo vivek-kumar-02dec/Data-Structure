5f1375d4f139f5a2e4961ca01f83bc4ce4eef41d
Vivek Kumar <vivek02122003@gmail.com>
2026-02-22 22:07:11 +0530

    Create 322_Coin_Change.cpp

---
+++ DP/322_Coin_Change.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> nums(amount+1,amount+1);
+        nums[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int coin:coins){
+                if(i-coin>=0){
+                    nums[i]=min(nums[i],1+nums[i-coin]);
+                }
+            }
+        }
+        return nums[amount]>amount?-1:nums[amount];
+    }
+};
