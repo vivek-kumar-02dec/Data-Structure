ffbfdb1ff570960fb520e97a88adfac350c5bebb
Vivek Kumar <vivek02122003@gmail.com>
2026-03-05 17:54:15 +0530

    Create 1758. Minimum Changes To Make Alternating Binary String.cpp

---
+++ Daily_Question/1758. Minimum Changes To Make Alternating Binary String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int n=s.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            count+=(s[i]^i)&1;
+        }
+        return min(count,n-count);
+
+    }
+};
