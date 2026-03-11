cf59317b6457b9117b1fd4bd0d2c0fb03bf7db8c
Vivek Kumar <vivek02122003@gmail.com>
2026-03-11 09:57:56 +0530

    Create 1009. Complement of Base 10 Integer.cpp

---
+++ Daily_Question/1009. Complement of Base 10 Integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0) return 1;
+        int sum=0;
+        int base=1;
+        while(n){
+            sum+=base*(!(n&1));
+            n>>=1;
+            base*=2;
+        }
+        return sum;
+    }
+};
