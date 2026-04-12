ec0d007e32204e8519a96aadac993ef9a7e6b8de
Vivek Kumar <vivek02122003@gmail.com>
2026-04-12 20:18:36 +0530

    Update 101. Symmetric Tree(recursive).cpp

--- Binary_Tree/101. Symmetric Tree(recursive).cpp
+++ Binary_Tree/101. Symmetric Tree(recursive).cpp
@@ -5,8 +5,8 @@ public:
             return true;
         if (!p || !q)
             return false;
-        if (p->val != q->val)
-            return false;
+        if (p->val != q->val){
+            return false;}
         return solve(p->left, q->right) && solve(p->right, q->left);
     }
     bool isSymmetric(TreeNode* root) {
