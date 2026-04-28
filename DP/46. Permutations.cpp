8cea9d749a08e3976c17f9e4c67086c703e83af8
Vivek Kumar <vivek02122003@gmail.com>
2026-04-28 21:55:15 +0530

    Create 46. Permutations.cpp

---
+++ DP/46. Permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void Findpermutaion(vector<int>& nums, vector<vector<int>>& ans, int idx) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            Findpermutaion(nums, ans, idx + 1);
+            swap(nums[idx], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        Findpermutaion(nums, ans, 0);
+        return ans;
+    }
+};
